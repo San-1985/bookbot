5f645407c0528e71cb97dc81b1260cc83102833d
Sandra Hirsch <sandra.hirsch@gmail.com>
2025-09-20 12:54:39 +0200

    finishing project with report

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 
-from stats import get_word_count, get_char_count, get_text
+from stats import get_word_count, get_char_count, get_text, sort_dict
+import sys
+
+def print_report(sorted_list, book, num_words):
+    report_start = f"\n============ BOOKBOT ============\nAnalyzing book found at {book}...\n----------- Word Count ----------\nFound {num_words} total words\n--------- Character Count -------"
+
+    report_end = "============= END ==============="
+    print(report_start)
+    for entry in sorted_list:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
+    print(report_end)
 
 def main():
-    text = get_text("./books/frankenstein.txt")
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    text = get_text(book)
     num_words = get_word_count(text)
     print(f"{num_words} words found in the document")
     char_count = get_char_count(text)
-    print(char_count)
+    sorted = sort_dict(char_count)
+    print_report(sorted, book, num_words)
+
 main()
 
--- stats.py
+++ stats.py
@@ -17,3 +17,13 @@ def get_char_count(text):
         else:
             char_count[char.lower()] = 1
     return char_count
+
+def sort_dict(d):
+    char_list = []
+    for k, v in d.items():
+        char_d = {"char": k, "num": v}
+        char_list.append(char_d)
+    def sort_on(items):
+        return items["num"]
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
