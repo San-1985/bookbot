f90f6d01de9f44c8746d5184ebf21d3edb939c58
Sandra Hirsch <sandra.hirsch@gmail.com>
2025-09-20 11:50:28 +0200

    implement character count

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
-from stats import get_word_count
+from stats import get_word_count, get_char_count, get_text
 
 def main():
-    num_words = get_word_count("./books/frankenstein.txt")
+    text = get_text("./books/frankenstein.txt")
+    num_words = get_word_count(text)
     print(f"{num_words} words found in the document")
-
+    char_count = get_char_count(text)
+    print(char_count)
 main()
 
--- stats.py
+++ stats.py
@@ -1,5 +1,19 @@
-def get_word_count(file):
+def get_text(file):
     with open(file) as f:
         text = f.read()
+    return text
+
+def get_word_count(text):
+    
     words = text.split()
     return len(words)
+
+def get_char_count(text):
+    
+    char_count = {}
+    for char in text:
+        if char.lower() in char_count:
+            char_count[char.lower()] += 1
+        else:
+            char_count[char.lower()] = 1
+    return char_count
